0b13496d034979e8553b77a5883ec818bdbe8118
Arisson Balbuena <arissonbalbuena2@gmail.com>
2026-04-21 01:46:09 -0300

    chore: update android apk url after premium mobile build

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const STORAGE_KEY = "painelPecuario.v2";
 const API_URL = "https://painel-pecuario-api.onrender.com";
 const MOBILE_APP_CONFIG = {
-  androidDownloadUrl: "https://expo.dev/artifacts/eas/cB3JaegfsXkawe9w4dX6T1.apk",
+  androidDownloadUrl: "https://expo.dev/artifacts/eas/vb8ogAiUpRsXSZbgX1iQA2.apk",
   iosDownloadUrl: "",
   androidFallbackUrl: "",
   iosFallbackUrl: "",
