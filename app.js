ba5ea8ed1d1e58dfb3bd5c23a41dd9793600f065
Arisson Balbuena <arissonbalbuena2@gmail.com>
2026-04-20 22:16:03 -0300

    chore: update android apk download url

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const STORAGE_KEY = "painelPecuario.v2";
 const API_URL = "https://painel-pecuario-api.onrender.com";
 const MOBILE_APP_CONFIG = {
-  androidDownloadUrl: "https://expo.dev/artifacts/eas/uQX7y4qdvXv8Y8VwhWM4kE.apk",
+  androidDownloadUrl: "https://expo.dev/artifacts/eas/fJMupNyksGi3fm32uzEAqE.apk",
   iosDownloadUrl: "",
   androidFallbackUrl: "",
   iosFallbackUrl: "",
